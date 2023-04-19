523160788931334a5fcc0c43998b3e20683dda53
Andrew Kyalo Nzioki <andrewkyalonzioki001.com>
2023-04-19 04:27:41 +0300

    updated

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,5 @@ module.exports = {
   presets: [
     "@babel/preset-env",
     ["@babel/preset-react", { runtime: "automatic" }],
-  ],
-
-  transform: {
-    "\\.[jt]sx?$": "babel-jest",
-    "\\.css$": "some-css-transformer",
-  },
+  ]
 };
