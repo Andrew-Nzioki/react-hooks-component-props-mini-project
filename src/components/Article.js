32959914b307960a9bd154fa57bf907930765b78
Andrew Kyalo Nzioki <andrewkyalonzioki001.com>
2023-04-21 11:24:07 +0300

    changed the article return to a the article element

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,13 +3,13 @@ import React from "react";
 function Article(props) {
   // console.log(props.post)
   return (
-    <div>
+    <article>
       <h3>{props.post} </h3>
       <small>
         {props.date} <span> {props.minutes} min read</span>
       </small>
       <p>{props.preview}</p>
-    </div>
+    </article>
   );
 }
 
